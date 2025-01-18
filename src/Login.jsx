93e7046cb34b7efdd1a205c0f7d8cc8315420a8f
bbungjin <leejintwo12@naver.com>
2025-01-19 07:53:16 +0900

    feat: 기능 구현

--- src/Login.jsx
+++ src/Login.jsx
@@ -9,43 +9,44 @@ const Login = () => {
     const navigate = useNavigate();
     const location = useLocation();
 
-    useEffect(() => {
-        const code = new URLSearchParams(location.search).get("code");
-        if (code) {
-            handleKakaoLogin(code);
-        }
-    }, [location]);
+    // useEffect(() => {
+    //     const code = new URLSearchParams(location.search).get("code");
+    //     if (code) {
+    //         handleKakaoLogin(code);
+    //     }
+    // }, [location]);
 
-    const handleKakaoLogin = async (code) => {
-        try {
-            const response = await axios({
-                method: 'post',
-                url: `${process.env.REACT_APP_API_URL}/api/v1/auth/signup`,
-                code: { code },
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }
-            });
+    // const handleKakaoLogin = async (code) => {
+        // try {
+        //     const response = await axios({
+        //         method: 'post',
+        //         url: `${process.env.REACT_APP_API_URL}/api/v1/auth/signup`,
+        //         code: { code },
+        //         headers: {
+        //             'Content-Type': 'application/json',
+        //             'Accept': 'application/json'
+        //         }
+        //     });
 
-            console.log('Server Response:', response);
+            // console.log('Server Response:', response);
 
-            if (response.data.resultCode === 200) {
-                localStorage.setItem('token', response.data.token);
-                navigate('/notice');
-            }
-        } catch (error) {
-            console.error('Login error:', error);
-            if (error.response) {
-                alert(`로그인 실패: ${error.response.data.message || '알 수 없는 오류가 발생했습니다.'}`);
-            } else if (error.request) {
-                window.location.href = `${process.env.REACT_APP_CLIENT_URL}/notice`;
-            } else {
-                console.error('Error message:', error.message);
-                alert('로그인 처리 중 오류가 발생했습니다.');
-            }
-        }
-    };
+        //     if (response.data.resultCode === 200) {
+        //         localStorage.setItem('token', response.data.token);
+        //         navigate('/notice');
+        //     }
+        // } catch (error) {
+        //     console.error('Login error:', error);
+        //     if (error.response) {
+        //         alert(`로그인 실패: ${error.response.data.message || '알 수 없는 오류가 발생했습니다.'}`);
+        //     } else if (error.request) {
+        //         window.location.href = `${process.env.REACT_APP_CLIENT_URL}/notice`;
+        //     } else {
+        //         console.error('Error message:', error.message);
+        //         alert('로그인 처리 중 오류가 발생했습니다.');
+        //     }
+    //     window.location.href = `${process.env.REACT_APP_CLIENT_URL}/notice`;
+    //     }
+    // };
 
     const handleLogin = () => {
         const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
@@ -60,7 +61,7 @@ const Login = () => {
                 {/* 카카오 로그인 버튼 */}
                 <div className="mt-8">
                     <button
-                        onClick={handleLogin}
+                        onClick={window.location.href = `${process.env.REACT_APP_CLIENT_URL}/notice`}
                         className="flex items-center justify-center bg-[#FEE500] hover:bg-[#FEE500]/90 text-[#391B1B] px-8 py-4 rounded-2xl shadow-md transition-all duration-200 hover:shadow-lg"
                     >
                         <img
